a659eeaff3921b34a44ad16272ca07ee506caa1e
Vitaly Shvetsov <nosferatu2995@mail.ru>
2018-02-23 18:20:55 +0700

    Add configs.

---
+++ Electrum_Wallet_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $HOME
+
+#Clone project
+if [ ! -d electrumx-wallet ]; then
+  git clone https://github.com/nosferatu500/electrumx-wallet.git
+fi
+
+set -e
+
+#Install dependencies for build project
+cd electrumx-wallet
+
+sudo apt-get install git pyqt4-dev-tools python-pip python-dev python-slowaes
+sudo python3.5 -m pip install pyasn1 pyasn1-modules pbkdf2 tlslite qrcode
+
+pyrcc4 icons.qrc -o gui/qt/icons_rc.py
+
+chmod 775 electrum-xvg
+
+sudo cp -r .electrum-xvg $HOME
+
+
+./electrum-xvg
